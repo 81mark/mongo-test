4f383ef33db391356216b6b18d4b91d4c139b63f
Mark Fernandez <mvf1981@gmail.com>
2019-12-01 17:56:27 +0100

    Setup backend server

---
+++ server/routes/api/posts.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+const db = process.env.DB;
+
+// Get Posts
+router.get('/', async (req, res) => {
+	const posts = await loadPostsCollection();
+	res.send(await posts.find({}).toArray());
+});
+
+// Add Posts
+router.post('/', async (req, res) => {
+	const posts = await loadPostsCollection();
+	await posts.insertOne({
+		text: req.body.text,
+		createdAt: new Date()
+	});
+	res.status(201).send();
+});
+
+// Delete Posts
+router.delete('/:id', async (req, res) => {
+	const posts = await loadPostsCollection();
+	await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+	res.status(200).send();
+});
+
+async function loadPostsCollection() {
+	const client = await mongodb.MongoClient.connect(`${db}`, {
+		useUnifiedTopology: true,
+		useNewUrlParser: true
+	});
+
+	return client.db('mongo-test-f8g3w').collection('posts');
+}
+
+module.exports = router;
